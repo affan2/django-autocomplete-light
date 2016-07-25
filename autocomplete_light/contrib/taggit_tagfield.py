c47675f634ab9c59a53c42041ae52949a346fc4b
Ahmed Salman <ahmed.salman.noor@gmail.com>
2016-07-25 14:23:46 +0500

    autocomplete independent of taggit

--- autocomplete_light/contrib/taggit_tagfield.py
+++ autocomplete_light/contrib/taggit_tagfield.py
@@ -1,23 +1,8 @@
-"""
-Support for django-taggit tags system. If using django-taggit, you **will**
-need this.
+from __future__ import unicode_literals
+from django import forms
+from django.utils.translation import ugettext as _
 
-Example usage::
-
-    from autocomplete_light.contrib.taggit_tagfield import TagField, TagWidget
-
-    class AppEditForm(forms.ModelForm):
-        tags = TagField(widget=TagWidget('TagAutocomplete'))
-
-.. Warning::
-    In this case, the tags field is a relation. Thus form.save() **must** be
-    called with commit=True.
-
-"""
-
-
-from taggit.forms import TagField as TaggitTagField
-from taggit.utils import edit_string_for_tags
+from general.utils import edit_string_for_tags, parse_tags
 
 from ..widgets import TextWidget
 
@@ -34,6 +19,18 @@ class TagWidget(TextWidget):
             except TypeError:
                 value = edit_string_for_tags(Topics.objects.filter(id__in=[o.tag.id for o in value]))
         return super(TagWidget, self).render(name, value, attrs)
+
+
+class TaggitTagField(forms.CharField):
+    widget = TagWidget
+
+    def clean(self, value):
+        value = super(TaggitTagField, self).clean(value)
+        try:
+            value = "%s," % value
+            return parse_tags(value)
+        except ValueError:
+            raise forms.ValidationError(_("Please provide a comma-separated list of tags."))
 
 
 class TagField(TaggitTagField):
