42bfa91fdfac36f42c2ca2dee55c8a626d92b557
Mohammad Abouchama <mohammadabouchama@gmail.com>
2019-07-11 15:39:43 +0300

    Remove test_project

--- test_project/linked_data/static/linked_data.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $(':input[name$=owner]').on('change', function() {
-        var prefix = $(this).getFormPrefix();
-        $(':input[name=' + prefix + 'test]').val(null).trigger('change');
-    });
-});
