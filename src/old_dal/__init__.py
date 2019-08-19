d23c9621cd77a56961d87004a8375718afb26b0b
Mohammad Abouchama <mohammadabouchama@gmail.com>
2019-08-19 16:57:44 +0400

    Update documentation, part 1

---
+++ src/old_dal/__init__.py
@@ -0,0 +1,7 @@
+"""
+Welcome to old django-autocomplete-light.
+
+This provides *some* of the old features provided by the v2 of this package.
+These features are needed for some other older packages, and no suitable replacements were found for
+these missing features.
+"""
